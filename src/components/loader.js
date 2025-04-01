1a7beb930457ed239415d11574ddb3711f65ffaf
alexdelise <alex.r.delise@gmail.com>
2025-03-31 22:52:40 -0400

    attempts to fix icon rendering error

--- src/components/loader.js
+++ src/components/loader.js
@@ -3,6 +3,7 @@ import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
 import anime from 'animejs';
 import { IconLoader } from '@components/icons';
+import '../styles/LogoFix.css';
 import styled from 'styled-components';
 import { theme, mixins } from '@styles';
 const { colors } = theme;
